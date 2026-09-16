c855b3c50cf2470f380b05fc0aa8f616d8455205
Pallavi <pallavigudla1313@gmail.com>
2026-09-16 20:23:52 +0530

    ✅ TYRES - Rating 809 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/easy/TYRES---Rating-809/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t-- >0){
+		    int a = sc.nextInt();
+		    if(a%4==0)
+		    System.out.println("no");
+		    else
+		    System.out.println("yes");
+		}
+	}
+}
